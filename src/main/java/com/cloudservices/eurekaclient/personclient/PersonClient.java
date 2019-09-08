48431dda09e2a349889f69b2d3f766e09c78c2b2
siddalinga nayak <siddalinganayak@siddalismacbook.home>
2019-09-07 21:22:23 -0400

    updated Person-User-Profile-api url

--- src/main/java/com/cloudservices/eurekaclient/personclient/PersonClient.java
+++ src/main/java/com/cloudservices/eurekaclient/personclient/PersonClient.java
@@ -25,12 +25,12 @@ public class PersonClient {
 
     @HystrixCommand(fallbackMethod = "getPerson")
     public PersonResponse getPersonDetails(int id) {
-        return restTemplate.getForObject("http://PERSON-USER-PROFILE-API/api/person/id?personId="+id, PersonResponse.class);
+        return restTemplate.getForObject("https://person-profile-api.herokuapp.com/api/person/id?personId="+id, PersonResponse.class);
     }
 
     @HystrixCommand(fallbackMethod = "getPersonList")
     public List<PersonResponse> getPersons() {
-        ResponseEntity<List<PersonResponse>> response = restTemplate.exchange("http://PERSON-USER-PROFILE-API/api/person/", HttpMethod.GET, null, new ParameterizedTypeReference<List<PersonResponse>>(){});
+        ResponseEntity<List<PersonResponse>> response = restTemplate.exchange("https://person-profile-api.herokuapp.com/api/person/", HttpMethod.GET, null, new ParameterizedTypeReference<List<PersonResponse>>(){});
         List<PersonResponse> personResponseList = response.getBody();
         return personResponseList;
     }
@@ -39,7 +39,7 @@ public class PersonClient {
     public String createPerson(PersonRequest personRequest) {
    //  return restTemplate.exchange("http://PERSON-USER-PROFILE-API/api/person/", HttpMethod.POST, null, String.class).getBody();
         String response = restTemplate.postForObject(
-                "http://PERSON-USER-PROFILE-API/api/person/",personRequest,
+                "https://person-profile-api.herokuapp.com/api/person/",personRequest,
                 String.class);
         return response;
     }
@@ -52,7 +52,7 @@ public class PersonClient {
 
     @HystrixCommand(fallbackMethod = "deletePersonByIdfallback")
     public String deletePersonById(int id) {
-        restTemplate.exchange("http://PERSON-USER-PROFILE-API/api/person/?personId="+id,HttpMethod.DELETE,null,String.class).getBody().toString();
+        restTemplate.exchange("https://person-profile-api.herokuapp.com/api/person/?personId="+id,HttpMethod.DELETE,null,String.class).getBody().toString();
         return "Deleted succesfully";
 
     }
@@ -60,7 +60,7 @@ public class PersonClient {
     public List<PersonResponse> filterPersonsByKey(Map<String,String> allParams) {
         ResponseEntity<List<PersonResponse>> response = null;
         for (Map.Entry<String, String> entry : allParams.entrySet()) {
-            response = restTemplate.exchange("http://PERSON-USER-PROFILE-API/api/person/search?"+entry.getKey()+"="+entry.getValue(),
+            response = restTemplate.exchange("https://person-profile-api.herokuapp.com/api/person/search?"+entry.getKey()+"="+entry.getValue(),
                     HttpMethod.GET, null, new ParameterizedTypeReference<List<PersonResponse>>(){});
         }
         List<PersonResponse> personResponseList = response.getBody();
